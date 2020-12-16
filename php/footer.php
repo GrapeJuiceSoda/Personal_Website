be88719e2146068b3de85434e2680ee2d8ce77dd
GrapeJuiceSoda <kendallcarmel@gmail.com>
2020-12-16 12:42:46 -0800

    front_page.html -> index.html

---
+++ php/footer.php
@@ -0,0 +1,3 @@
+<?php
+echo '<a href="http://9front.org/"><img src="./pictures/power36.gif" alt="powered by plan9" border="0"></a>';
+?>
---
+++ php/top_menu.php
@@ -0,0 +1,13 @@
+<?php
+echo '
+<a href="front_page.html">front</a> | 
+<a href="plan9.html">p9</a> | 
+<a href="random.html">b</a> | 
+<a href="chan.html">g</a> | 
+<a href="programming.html">λ</a> | 
+<a href="chan.html">new</a> | 
+<a href="chan.html">ab</a> | 
+<a href="chan.html">qa</a> | 
+<a href="http://cat-v.org">cat-v.org</a> |
+';
+?>
